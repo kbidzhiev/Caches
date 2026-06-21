8a99c4a3bac22ad9fc3c91783ae1279c2311a92b
Your Name <you@example.com>
2026-06-21 12:47:53 +0200

    lru from lectures

--- LRU/LRU.hpp
+++ LRU/LRU.hpp
@@ -6,24 +6,25 @@
 namespace cache {
 
 template <typename T, typename KeyT = int> class LRU_t {
+  size_t sz_;
   std::list<T> cache_;
 
   using ListIt = typename std::list<T>::iterator;
   std::unordered_map<T, ListIt> hash_;
 
 public:
-  LRU_t(size_t sz) : hash_{sz} {}
+  LRU_t(size_t sz) : sz_{sz} {}
 
-  bool full() const { return cache_.size() == hash_.size(); };
+  bool full() const { return cache_.size() == sz_; };
 
   template <typename F> bool lookup_update(KeyT key, F slow_get_page) {
     auto hit = hash_.find(key);
     if (hit == hash_.end()) { // not found
       if (full()) {
-        hash_.erase(cache_.back().id);
+        hash_.erase(cache_.back());
         cache_.pop_back();
       }
-      cache_push_front(slow_get_page(key));
+      cache_.push_front(slow_get_page(key));
       hash_[key] = cache_.begin();
       return false;
     }
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,27 @@
 #include "LRU.hpp"
+#include <cassert>
 #include <iostream>
 
-int main() {
-  std::string str = hello();
+int slow_get_page(int key) { return key; }
 
-  std::cout << str << std::endl;
+
+int main() {
+  size_t hits{};
+  size_t sz;
+  int nelts;
+
+  std::cin >> sz >> nelts;
+
+  cache::LRU_t<int> lru{sz};
+
+  for (int i = 0; i < nelts; ++i) {
+    int p;
+    std::cin >> p;
+    assert(std::cin.good());
+    if (lru.lookup_update(p, slow_get_page))
+      hits += 1;
+  }
+
+  std::cout << "hits: " << hits << std::endl;
+  return 0;
 }
