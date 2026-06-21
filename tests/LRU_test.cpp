8b06ad7303556811b57f8e5f623a25ef125e355c
Your Name <you@example.com>
2026-06-21 14:59:49 +0200

    new tests

--- tests/LRU_test.cpp
+++ tests/LRU_test.cpp
@@ -1,17 +1,80 @@
 #include "LRU.hpp"
 #include <gtest/gtest.h>
+#include <vector>
 
 
+struct page_t{
+  int id;
+};
 
-int slow_get_page(int key) {return key;}
+page_t slow_get_page(int key) { return page_t{key}; }
 
-
-// Demonstrate some basic assertions.
-TEST(LRU, ctr) {
-  cache::LRU_t<int> lru{3};
+TEST(LRU, no_hit) {
+  cache::LRU_t<page_t> lru{3};
 
   // cache is empty, no hits
-  EXPECT_FALSE(lru.lookup_update(1, slow_get_page));
-  EXPECT_FALSE(lru.lookup_update(2, slow_get_page));
-  EXPECT_FALSE(lru.lookup_update(3, slow_get_page));
+  EXPECT_FALSE(lru.lookup_update(1, slow_get_page ));
 }
+
+TEST(LRU, one_hit) {
+  cache::LRU_t<page_t> lru{3};
+
+  EXPECT_FALSE(lru.lookup_update(1, slow_get_page));
+  EXPECT_TRUE(lru.lookup_update(1, slow_get_page)); // cache hit
+}
+
+TEST(LRU, is_full) {
+  cache::LRU_t<page_t> lru{2};
+
+  lru.lookup_update(1, slow_get_page); // [1]
+  lru.lookup_update(1, slow_get_page); // [1]
+  EXPECT_FALSE(lru.full());
+
+  lru.lookup_update(2, slow_get_page); // [2, 1]
+  EXPECT_TRUE(lru.full());
+}
+
+TEST(LRU, no_eviction) {
+  cache::LRU_t<page_t> lru{3};
+
+  EXPECT_FALSE(lru.lookup_update(1, slow_get_page)); // [1]
+  EXPECT_FALSE(lru.lookup_update(2, slow_get_page)); // [2, 1]
+  EXPECT_TRUE(lru.lookup_update(1, slow_get_page));  // [1, 2]
+  EXPECT_FALSE(lru.lookup_update(3, slow_get_page)); // [3, 1, 2]
+  EXPECT_TRUE(lru.lookup_update(1, slow_get_page));  // [1, 3, 2]
+  EXPECT_TRUE(lru.lookup_update(2, slow_get_page));  // [2, 1, 3]
+}
+
+TEST(LRU, eviction) {
+  cache::LRU_t<page_t> lru{2};
+
+  lru.lookup_update(1, slow_get_page); // [1]
+  lru.lookup_update(2, slow_get_page); // [2, 1]
+  lru.lookup_update(3, slow_get_page); // [3, 2]
+
+  EXPECT_FALSE(lru.lookup_update(1, slow_get_page)); // 1 is not present before lookup
+}
+
+TEST(LRU, example_from_lecture) {
+  cache::LRU_t<page_t> lru{2};
+
+  std::vector<int> data_id = {1, 2, 1, 2, 1, 2};
+  size_t hits{};
+  for (auto el : data_id) {
+    if (lru.lookup_update(el, slow_get_page))
+      hits += 1;
+  }
+
+  EXPECT_EQ(4, hits);
+}
+
+TEST(LRU, non_default_key) {
+
+
+}
+
+
+
+
+
+
